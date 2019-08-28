017baaa3010da4dd21b677be4393157fcb9185ea
ageng-windows <ageng133@gmail.com>
2019-08-28 14:26:40 +0700

    config me

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Bangkok");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pengukuran2/';
+$config['base_url'] = 'http://localhost:90/pengukuran-360/';
 
 /*
 |--------------------------------------------------------------------------
