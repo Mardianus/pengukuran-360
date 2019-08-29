6fecb346fad1b4ec5abb3b68349ac84b1805df4a
Mardianus <mardianus1997@gmail.com>
2019-08-30 00:56:29 +0700

    asdfg

--- application/views/Admin/V_KamusKompetensi.php
+++ application/views/Admin/V_KamusKompetensi.php
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
-<form action="<?= base_url('Admin/KamusKompetensi/Simpan/'); ?>" method="POST">
-    <table class="table table-bordered table-hover table-striped" style="width:100%;">
-        <thead>
-            <tr>
-                <th class="text-center text-uppercase">
-                    no.
-                </th>
-                <th class="text-center text-uppercase">
-                    kode<br>kompetensi
-                </th>
-                <th class="text-center text-uppercase">
-                    Nama<br>Kompetensi
-                </th>
-                <th class="text-center text-uppercase">
-                    Level
-                </th>
-                <th class="text-center text-uppercase">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</form>
-=======
+
 <div class="table-responsive">
     <button class="btn btn-success"> <i class="fa fa-plus"></i> Tambah Data</button>
     <table class="table table-bordered table-hover table-striped">
