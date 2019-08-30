414faff0f1d08e49564b4e8114fefa8c5590618a
Mardianus <mardianus1997@gmail.com>
2019-08-30 15:53:13 +0700

    duda

--- application/models/Admin/M_GAPkompetensi.php
+++ application/models/Admin/M_GAPkompetensi.php
@@ -14,7 +14,16 @@
 class M_GAPkompetensi extends CI_Model{
     
     function index (){
-        
+        $exec = $this->db->select('pegawai.nik,pegawai.nama,jabatan.nama_jabatan,jabatan.divisi,jabatan.unit_kerja,level_jabatan.`level`,kelompok_jabatan.kelompok')
+                ->from('pegawai')
+                ->join('jabatan', 'pegawai.kode_jabatan = jabatan.kode_jabatan', 'LEFT')
+                ->join('matrik_jabatan', 'jabatan.kode_matrik = matrik_jabatan.kode_matrik', 'LEFT')
+                ->join('level_jabatan', 'matrik_jabatan.id_level = level_jabatan.id', 'LEFT')
+                ->join('kelompok_jabatan', 'matrik_jabatan.id_kelompok = kelompok_jabatan.id', 'LEFT')
+                ->where('pegawai.status_active', 1)
+                ->get()
+                ->result();
+        return $exec;
     }
     
 }
--- application/views/Admin/V_GAPkompetensi.php
+++ application/views/Admin/V_GAPkompetensi.php
@@ -1,3 +1,40 @@
+<div class="row">
+    <div class="col-md-4 form-horizontal">
+        <div class="form-group">
+            <label class="text-uppercase col-sm-4">Nama Pegawai <small class="text-danger">*</small></label>
+            <div class="col-sm-8">
+                <select class="form-control text-uppercase" name="pegawai" required="">
+                    <option value="" class="text-uppercase">pilih Nama Pegawai</option>
+                    <?php foreach ($value as $pegawai) { ?>
+                        <option value="<?= $pegawai->nik ?>"><?= $pegawai->nama ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="text-uppercase col-sm-4">nik</label>
+            <div class="col-sm-8">
+                <p id="nikpeg">89798779</p>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="text-uppercase col-sm-4">jabatan</label>
+            <div class="col-sm-8">
+                <p id="jabatan">dirut</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+
+    </div>
+</div>
+<div class="form-group">
+
+</div>
 <script>
     document.onreadystatechange = () => {
         if (document.readyState === 'complete') {
