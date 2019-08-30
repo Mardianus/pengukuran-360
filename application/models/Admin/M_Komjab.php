67862512f8309e0a8a53e1fc435d2ca5848ab875
ageng-windows <ageng133@gmail.com>
2019-08-31 01:14:30 +0700

    komjab nih bikin form

--- application/models/Admin/M_Komjab.php
+++ application/models/Admin/M_Komjab.php
@@ -14,6 +14,11 @@
 class M_Komjab extends CI_Model{
     
     function index(){
-        
+        return $this->db->select('*')
+                        ->from('jabatan a')
+                        ->join('matrik_jabatan b', 'a.kode_matrik = b.kode_matrik')
+                        ->join('level_jabatan c', 'b.id_level = c.id')
+                        ->get()
+                        ->result();
     }
 }
